2153c65eab6cbf99ee5b178068aa23c912253fe7
Young <yi5oyu@gmail.com>
2025-01-21 18:41:41 +0900

    remove: axios 테스트 삭제 (#4)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,8 @@
-import { useState } from 'react'
 import Homepage from './components/pages/Home'
 
-import axios from 'axios'
-
-function App() {
-  const [data, setData] = useState('')
-
-  const fetchModels = () => {
-    axios
-      .get('http://localhost:8888/api/chat/models')
-      .then((response) => {
-        setData(response.data)
-      })
-      .catch((error) => {
-        console.error('에러:', error)
-      })
-  }
-
+const App = () => {
   return (
     <>
-      <button onClick={fetchModels}>버튼</button>
-      <div>{data ? JSON.stringify(data, null, 2) : 'Click to fetch models'}</div>
       <Homepage />
     </>
   )

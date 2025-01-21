e2bd11b235c31ef9614125154caa10744123cdb1
Young <yi5oyu@gmail.com>
2025-01-22 03:11:23 +0900

    feat: ai 채팅 구현 (#6)

--- frontend/src/components/layout/Screen.jsx
+++ frontend/src/components/layout/Screen.jsx
@@ -4,15 +4,29 @@ import MarkDownInputBox from './MarkDownInputBox'
 import MarkdownPreview from './MarkdownPreview'
 import Questionbar from './Questionbar'
 import ChatBox from './ChatBox'
+import axios from 'axios'
 
 const Screen = () => {
   const [markdownText, setMarkdownText] = useState('')
   const [questionText, setQuestionText] = useState('')
   const [messages, setMessages] = useState([])
 
-  const handleSendMessage = () => {
+  // ai 채팅
+  const handleSendMessage = async () => {
     if (questionText.trim()) {
-      setMessages((m) => [...m, questionText])
+      setMessages((m) => [...m, { role: 'user', content: questionText }])
+      try {
+        let response = await axios.post('http://localhost:8888/api/chat/lmstudio', {
+          model: 'llama-3.2-korean-blossom-3b',
+          messages: [{ role: 'user', content: questionText }],
+        })
+
+        let aiResponse = response.data.choices[0]?.message?.content || 'AI 응답없음'
+        setMessages((m) => [...m, { role: 'assistant', content: aiResponse }])
+      } catch (error) {
+        console.error('에러:', error)
+        setMessages((m) => [...m, { role: 'assistant', content: '에러' }])
+      }
       setQuestionText('')
     }
   }

42989d667fb545e0419d043c8b062c73352a760b
Young <yi5oyu@gmail.com>
2025-01-21 16:55:00 +0900

    feat: 대화창 구현 (#3)
     - 질문창 질문 대화창에 입력 기능

---
+++ frontend/src/components/layout/ChatBox.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import { Box, Flex } from '@chakra-ui/react'
+
+const ChatBox = ({ messages }) => {
+  return (
+    <>
+      <Box fontWeight="bold" mb="2">
+        메시지
+      </Box>
+      {messages.length > 0 ? (
+        messages.map((m, index) => (
+          <Box key={index} p="2" mb="2" bg="gray.50" borderRadius="md">
+            {m}
+          </Box>
+        ))
+      ) : (
+        <></>
+      )}
+    </>
+  )
+}
+
+export default ChatBox

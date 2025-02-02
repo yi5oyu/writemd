6bf73afc2a082131ed67806df8581aa5ac168c03
Young <yi5oyu@gmail.com>
2025-02-02 17:31:18 +0900

    feat: repository(JPA) 구현 (#8)

---
+++ backend/src/main/java/com/writemd/backend/repository/TextRepository.java
@@ -0,0 +1,10 @@
+package com.writemd.backend.repository;
+
+import com.writemd.backend.entity.Chats;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TextRepository extends JpaRepository<Chats, Long> {
+
+}
---
+++ backend/src/main/java/com/writemd/backend/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.writemd.backend.repository;
+
+import com.writemd.backend.entity.Users;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<Users, Long> {
+    Optional<Users> findByGithubId(String githubId);
+}

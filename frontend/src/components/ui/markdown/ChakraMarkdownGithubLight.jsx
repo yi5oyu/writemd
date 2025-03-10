c9e506fae04d3e0688a28ff673e5a55c86131d66
Young <yi5oyu@gmail.com>
2025-03-10 18:49:00 +0900

    feat: markdown 스타일 변경 (#17)
    - remark-emoji 적용
    - ToolBox 이모지 상자 제거
    - 체크리스트 스타일 변경(ul)
    - 인라인코드 스타일 변경(여백, 마진)

--- frontend/src/components/ui/markdown/ChakraMarkdownGithubLight.jsx
+++ frontend/src/components/ui/markdown/ChakraMarkdownGithubLight.jsx
@@ -246,7 +246,15 @@ const ChakraMarkdownGithubLight = {
 
   // 인라인 코드
   code: (props) => (
-    <Code fontSize="sm" borderRadius="md" bg="gray.100" px="0.2em" py="0.1em" {...props} />
+    <Code
+      display="inline"
+      fontSize="sm"
+      borderRadius="md"
+      p="2.5px 4.5px"
+      whiteSpace="pre-wrap"
+      wordBreak="break-all"
+      {...props}
+    />
   ),
 
   // 코드 블록
@@ -310,24 +318,29 @@ const ChakraMarkdownGithubLight = {
   thematicBreak: (props) => <Divider my={4} {...props} />,
 
   // 목록
-  ul: (props) => (
-    <UnorderedList
-      styleType="disc"
-      sx={{
-        '& ul': {
-          listStyleType: 'circle !important',
-          ml: 6,
-        },
-        '& ul ul': {
-          listStyleType: 'square !important',
-          ml: 6,
-        },
-      }}
-      ml={6}
-      mb={3}
-      {...props}
-    />
-  ),
+  ul: ({ className, ...props }) => {
+    const isChecklist = className && className.includes('contains-task-list')
+    return (
+      <UnorderedList
+        styleType={isChecklist ? 'none' : 'disc'}
+        sx={{
+          '& ul': {
+            listStyleType: isChecklist ? 'none !important' : 'circle !important',
+            ml: isChecklist ? 0 : 6,
+          },
+          '& ul ul': {
+            listStyleType: isChecklist ? 'none !important' : 'square !important',
+            ml: isChecklist ? 0 : 6,
+          },
+        }}
+        ml={isChecklist ? 0 : 6}
+        mb={3}
+        className={className}
+        {...props}
+      />
+    )
+  },
+
   // 순서 있는 목록 (ol)
   ol: (props) => (
     <OrderedList
@@ -347,6 +360,7 @@ const ChakraMarkdownGithubLight = {
       {...props}
     />
   ),
+
   // 목록 아이템
   li: (props) => <ListItem mb={2} {...props} />,
 
--- frontend/src/features/markdown/MarkdownPreview.jsx
+++ frontend/src/features/markdown/MarkdownPreview.jsx
@@ -4,6 +4,7 @@ import ReactMarkdown from 'react-markdown'
 import ChakraUIRenderer from 'chakra-ui-markdown-renderer'
 import remarkGfm from 'remark-gfm'
 import remarkSlug from 'remark-slug'
+import remarkEmoji from 'remark-emoji'
 import rehypeRaw from 'rehype-raw'
 import ChakraMarkdownGithubLight from '../../components/ui/markdown/ChakraMarkdownGithubLight'
 
@@ -20,7 +21,7 @@ const MarkdownPreview = ({ markdownText }) => {
     >
       <ReactMarkdown
         components={ChakraUIRenderer(ChakraMarkdownGithubLight)}
-        remarkPlugins={[remarkGfm, remarkSlug]}
+        remarkPlugins={[remarkGfm, remarkSlug, remarkEmoji]}
         rehypePlugins={[rehypeRaw]}
       >
         {markdownText}

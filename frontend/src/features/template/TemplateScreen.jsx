4bc52090459f0bb10480b9ab83ae3595e5f47698
Young <yi5oyu@gmail.com>
2025-03-27 15:37:40 +0900

    design: NewTemplate 컴포넌트 UI/Contents 데이터 생성 (#21)

--- frontend/src/features/template/TemplateScreen.jsx
+++ frontend/src/features/template/TemplateScreen.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { Box, Button, Flex } from '@chakra-ui/react'
+import NewTemplate from './NewTemplate'
 
 const TemplateScreen = ({ screen }) => {
   return (
@@ -7,18 +8,20 @@ const TemplateScreen = ({ screen }) => {
       direction="column"
       borderRadius="md"
       boxShadow="md"
-      bg="white"
+      bg="gray.100"
       h={screen ? 'calc(100vh - 145px)' : 'calc(100vh - 90px)'}
     >
       <Flex p="15px" gap="2">
         <Button w="100px" boxShadow="md" bg="white" color="blue.500">
-          새 템플릿
-        </Button>
-        <Button w="100px" boxShadow="md" bg="white" color="blue.500">
           목록
         </Button>
+        <Button w="100px" boxShadow="md" bg="white" color="blue.500">
+          새 템플릿
+        </Button>
       </Flex>
-      <Box mx="15px" mb="15px" h="100%" borderRadius="md" boxShadow="md" bg="gray.100"></Box>
+      <Box mx="15px" mb="15px" h="100%" borderRadius="md" boxShadow="md" bg="white">
+        <NewTemplate />
+      </Box>
     </Flex>
   )
 }

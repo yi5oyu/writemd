0f64eff26593e38d62cf4b51c7e029edc7c51eb8
Young <yi5oyu@gmail.com>
2025-04-23 13:18:19 +0900

    design: 스크롤바 UI/화면 위치 설정 (#25)

--- frontend/src/features/chat/ChatBox.jsx
+++ frontend/src/features/chat/ChatBox.jsx
@@ -1,3 +1,4 @@
+import React, { useLayoutEffect, useRef } from 'react'
 import { Flex } from '@chakra-ui/react'
 import LoadingSpinner from '../../components/ui/spinner/LoadingSpinner'
 import ContentsSpinner from '../../components/ui/spinner/ContentsSpinner'
@@ -5,8 +6,19 @@ import PreviewBox from '../markdown/PreviewBox'
 import UserChatMessage from './UserChatMessage'
 
 const ChatBox = ({ messages, chatLoading, messageLoading, screen }) => {
+  const scrollRef = useRef(null)
+
+  // 맨아래
+  useLayoutEffect(() => {
+    if (scrollRef.current) {
+      const scrollElement = scrollRef.current
+      scrollElement.scrollTop = scrollElement.scrollHeight
+    }
+  }, [messages])
+
   return (
     <Flex
+      ref={scrollRef}
       flexDirection="column"
       bg="white"
       boxShadow="md"
@@ -14,6 +26,35 @@ const ChatBox = ({ messages, chatLoading, messageLoading, screen }) => {
       overflowY="auto"
       sx={{
         scrollbarGutter: 'stable',
+
+        '&::-webkit-scrollbar': {
+          width: '10px',
+        },
+        '&::-webkit-scrollbar-track': {
+          background: 'transparent',
+          borderRadius: '5px',
+        },
+        '&::-webkit-scrollbar-thumb': {
+          background: 'transparent',
+          borderRadius: '10px',
+          borderWidth: '2px',
+          borderStyle: 'solid',
+          borderColor: 'transparent',
+          backgroundClip: 'padding-box',
+        },
+
+        '&::-webkit-scrollbar:hover': {
+          '&::-webkit-scrollbar-thumb': {
+            background: 'rgba(0, 0, 0, 0.5)',
+          },
+          '&::-webkit-scrollbar-track': {
+            background: 'rgba(0, 0, 0, 0.1)',
+          },
+        },
+
+        '&::-webkit-scrollbar-thumb:hover': {
+          background: 'rgba(0, 0, 0, 0.65)',
+        },
       }}
       h={screen ? 'calc(100vh - 145px)' : 'calc(100vh - 99px)'}
       filter={chatLoading ? 'blur(4px)' : 'none'}
@@ -23,11 +64,7 @@ const ChatBox = ({ messages, chatLoading, messageLoading, screen }) => {
       {messages.length > 0 &&
         messages.map((m, index) =>
           m.role === 'user' ? (
-            <UserChatMessage
-              key={index} // key는 map의 직접적인 자식 요소에 필요
-              content={m.content}
-              lines={3}
-            />
+            <UserChatMessage key={index} content={m.content} lines={3} />
           ) : (
             <PreviewBox markdownText={m.content} chat={true} />
           )

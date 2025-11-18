f035ad2eee0495e76d366a93ac359218dcf7f55f
Young <yi5oyu@gmail.com>
2025-11-18 16:12:25 +0900

    feat: Redis 기반 토큰 관리 구현 (#35)
    - TokenDTO, TokenRedisService
    - Refresh Token 저장/조회
    - 로그아웃, 토큰 블랙리스트 추가

---
+++ backend/src/main/java/com/writemd/backend/dto/TokenDTO.java
@@ -0,0 +1,14 @@
+package com.writemd.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenDTO {
+
+    private String token;
+    private long issuedAt;
+}

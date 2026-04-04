80fd9cf25d5f2c92cfbe5cc543eb0dd11b2bfd0d
Young <yi5oyu@gmail.com>
2026-04-05 00:08:37 +0900

    test: 레디스 캐싱 설정 (#47) [deploy]

--- backend/src/main/java/com/writemd/backend/config/RedisConfig.java
+++ backend/src/main/java/com/writemd/backend/config/RedisConfig.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
@@ -15,7 +16,7 @@ import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-//@EnableCaching
+@EnableCaching
 public class RedisConfig {
 
     @Bean

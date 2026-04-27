a873e953563302c294f3817256b412f07c2bb4f3
Young <yi5oyu@gmail.com>
2026-04-27 15:47:06 +0900

    fix: SecurityConfig 문법 오류 수정 (#48) [hotfix]
    - 부하 테스트 이중 검사 제거

--- backend/src/main/java/com/writemd/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/writemd/backend/config/SecurityConfig.java
@@ -9,7 +9,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -80,19 +79,16 @@ public class SecurityConfig {
 
             .authorizeHttpRequests((requests) -> requests
                 .dispatcherTypeMatchers(DispatcherType.ASYNC).permitAll()
-                .requestMatchers("/api/guest/**").permitAll()
+
                 // 인증 권한
-                .requestMatchers("/**").access((authentication, context) -> {
-                    String userKey = context.getRequest().getHeader("X-Load-Test-Key");
-                    if (loadTestKey != null && loadTestKey.equals(userKey)) {
-                        return new AuthorizationDecision(true);
-                    }
-                    return null;
-                })
-                .requestMatchers("/redis/**", "/mcp/**", "/error", "/oauth2/**", "swagger-ui.html", "/v1/**",
-                    "/swagger-ui/**", "/login/oauth2/**", "/actuator/**", "/logout", "/v1/**", "/sse", "/api/config/**")
-                .permitAll()
+                .requestMatchers(
+                    "/api/guest/**", "/api/config/**",
+                    "/redis/**", "/mcp/**", "/error", "/oauth2/**", "swagger-ui.html",
+                    "/v1/**", "/swagger-ui/**", "/login/oauth2/**", "/actuator/**", "/logout", "/sse"
+                ).permitAll()
+
                 .requestMatchers("/profile/**", "/api/**", "/h2-console/**").authenticated()
+
                 .anyRequest().authenticated())
 
             // OAuth2 로그인
